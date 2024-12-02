d589c5d87ffb0f7234b6237115a04cbc11d6ffad
negarhdr <negarheydari71@gmail.com>
2024-12-02 10:45:42 +0100

    Refactor run_kitti.py

--- scripts/run_kitti.py
+++ scripts/run_kitti.py
@@ -10,23 +10,24 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import trackeval  
 from scripts.base_runner import BaseRunner
+from trackeval import metrics, datasets, Evaluator
+
 
 class KittiRunner(BaseRunner):
     def __init__(self):
         """
         Initialize KittiRunner with specific default configurations.
         """
-        default_eval_config = trackeval.Evaluator.get_default_eval_config()
+        default_eval_config = Evaluator.get_default_eval_config()
         default_eval_config['DISPLAY_LESS_PROGRESS'] = False
-        default_dataset_config = trackeval.datasets.Kitti2DBox.get_default_dataset_config()
+        default_dataset_config = datasets.Kitti2DBox.get_default_dataset_config()
         default_metrics_config = {'METRICS': ['HOTA', 'CLEAR', 'Identity', 'MaxSim']}
         super().__init__(default_eval_config, default_dataset_config, default_metrics_config)
 
 if __name__ == '__main__':
     runner = KittiRunner()
     runner.run(
-        dataset_cls = trackeval.datasets.Kitti2DBox,
-        metric_classes = [trackeval.metrics.HOTA, trackeval.metrics.CLEAR, trackeval.metrics.Identity, trackeval.metrics.MaxSim]
+        dataset_cls = datasets.Kitti2DBox,
+        metric_classes = [metrics.HOTA, metrics.CLEAR, metrics.Identity, metrics.MaxSim]
     )
